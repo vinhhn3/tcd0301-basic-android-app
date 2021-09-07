de6f8d9b4ffe5661663e3d3dfca28702a1bec415
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-09-07 11:52:48 +0700

    Implement Insert button with validation\

--- app/src/main/java/com/example/tcd0301basicandroidapp/MainActivity.java
+++ app/src/main/java/com/example/tcd0301basicandroidapp/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.tcd0301basicandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText name, contact, dob;
+    Button btnInsert, btnView, btnDelete, btnUpdate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        contact = findViewById(R.id.contact);
+        dob = findViewById(R.id.dob);
+
+        btnInsert = findViewById(R.id.btnInsert);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnView = findViewById(R.id.btnView);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameText = name.getText().toString();
+                String contactText = contact.getText().toString();
+                String dobText = dob.getText().toString();
+
+                if (!nameText.isEmpty() && !contactText.isEmpty() & !dobText.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Inserted Successfully !!!", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Inserted Failed !!!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
